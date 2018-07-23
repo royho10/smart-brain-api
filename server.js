4e9d4c58d5ecd569aa9295b5e4a0863bbada4122
Roy Honigman <eparis@atlassian.com>
2018-07-23 17:29:19 +0300

    knex connection

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-trapezoidal-53110',
-    user : 'postgres',
-    password : 'language',
-    database : 'smart-brain'
+    host : process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
